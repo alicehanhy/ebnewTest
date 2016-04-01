93d88e4623b8035a4d947c7c9deec57e452d8f1c
alicehanhy <alicehanhy@126.com>
2016-04-01 15:57:25 +0800

    1、数据驱动的几种方式封装成一个util 2、连接以及输入输出流的建立：java.io与java.nio的区别

--- src/test/java/cn/ebnew/utils/CreateConn.java
+++ src/test/java/cn/ebnew/utils/CreateConn.java
@@ -1,30 +1,38 @@
 package cn.ebnew.utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
 
 public class CreateConn {
 	private String host;
 	private int port;
 	Socket so;
+
 	public String getHost() {
 		return host;
 	}
+
 	public void setHost(String host) {
 		this.host = host;
 	}
+
 	public int getPort() {
 		return port;
 	}
+
 	public void setPort(int port) {
 		this.port = port;
 	}
 
-	public Socket createConnection(){
-		if (this.so==null) {
+	// 创建socket连接
+	public Socket createConnection() {
+		if (this.so == null) {
 			try {
-				this.so=new Socket(getHost(),getPort());
+				this.so = new Socket(getHost(), getPort());
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
@@ -33,5 +41,10 @@ public class CreateConn {
 		}
 		return so;
 	}
-	
+	/*
+	 * 创建write流 从内存向硬盘存数据 - O 从硬盘向内存存数据 - I
+	 */
+
+	// 创建read流
+
 }
--- src/test/java/cn/ebnewTest/login/functionLoginTest.java
+++ src/test/java/cn/ebnewTest/login/functionLoginTest.java
@@ -3,40 +3,62 @@ package cn.ebnewTest.login;
 import org.testng.annotations.Test;
 
 import cn.ebnew.utils.CreateConn;
+import cn.ebnew.utils.StaticProvider;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
+
+import java.net.Socket;
+
+import javax.sql.ConnectionPoolDataSource;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 
 public class functionLoginTest {
-  @Test
-  //正常登录，正确的用户名和密码
-  public void login1() {
-	  
-  }
-  @Test
-  //异常登录：正确的用户名，错误的密码
-  public void login2(){
-	  
-  }
-  @Test
-  //异常登录：正确的用户名，密码为空
-  public void login3(){
-	  
-  }
-  @Parameters
-  
-  @BeforeTest
-  public void BeforeTest(){
-	  CreateConn conn=new CreateConn();
-//	  conn.setHost(host);
-//	  conn.setPort(port);
-  }
-  @AfterTest
-  public void AfterTest(){
-	  
-  }
 
+	@Test(dataProvider = "ConnctionDataProvider", dataProviderClass = StaticProvider.class)
+	// 正常登录，正确的用户名和密码
+	public void login1(String host, int port) {
+		Socket socket=null;
+		CreateConn conn = new CreateConn();
+		conn.setHost(host);
+		conn.setPort(port);
+		socket=conn.createConnection();
+		System.out.println("login1");
+	}
+
+	@Test
+	// 异常登录：正确的用户名，错误的密码
+	public void login2() {
+		System.out.println("login2");
+	}
+
+	@Test
+	// 异常登录：正确的用户名，密码为空
+	public void login3() {
+		System.out.println("login3");
+	}
+
+	@BeforeTest
+	public void BeforeTest() {
+		System.out.println("beforeTest");
+	}
+
+	@AfterTest
+	public void AfterTest() {
+		System.out.println("afterTest");
+	}
+
+	@BeforeMethod
+	public void BeforeMethod() {
+		System.out.println("beforeMethod");
+	}
+
+	@AfterMethod
+	public void AfterMethod() {
+		System.out.println("afterMethod");
+	}
 }
